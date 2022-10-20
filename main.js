534d9b251341da2ce389f01f12acbabeb307fbeb
JahirRamirez <jramirez35@ucol.mx>
2022-10-19 21:39:46 -0500

    Listas Enlazadas

---
+++ main.js
@@ -0,0 +1,29 @@
+class Inventario{
+    constructor(){
+        this.array=[];
+    }
+    añadir(){
+
+    }
+    buscar(){
+
+    }
+    borrar(){
+
+    }
+    insertar(){
+        
+    }
+    recuperarListado(){
+
+    }
+}
+
+class Producto{
+    constructor(nombre, codigo, costo, cantidad){
+        this.nombre=nombre;
+        this.codigo=codigo;
+        this.costo=costo;
+        this.cantidad=cantidad;
+    }
+}
